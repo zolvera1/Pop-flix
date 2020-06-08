48142eee54cc2b5f0dc1b331baa268309b9cf455
Jean Morillo sanchez <morillo30@gmail.com>
2020-06-08 15:25:42 -0400

    implemented search bar with the movie title endpoint

--- src/App.js
+++ src/App.js
@@ -1,29 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-       <div class='topnav'>
-                <input type="text" placeholder="Search..." name='search'></input>
+// import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './styles.css';
+import './css/search.css'
+import Autocomplete from "./services/search";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+
+    fetch('https://casecomp.konnectrv.io/movie')
+      .then(response => response.json())
+      .then(dataArray => {
+
+
+        let result = dataArray.map(data => data.title)
+        this.setState({ data: result });
+        console.log(result);
+      }
+      );
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className='topnav'>
+          <input type="text" placeholder="Search..." name='search'></input>
         </div>
-        <div class='nav-filters'>
-            <h4>Ratings</h4>
-            <h4>Streaming Service</h4>
-            <h4>Genre</h4>
-            <h4>Length</h4>
-            <h4>Media Type</h4>
+
+        <div className="center">
+
+          <div className="contanier">
+            <Autocomplete
+              suggestions={this.state.data}
+            />
+          </div>
+
+          <button type="button" className="btn btn-default" id="search-btn">Success</button>
         </div>
-        <div class = "header-container"> 
-            <h1>Movie Collection</h1>
+        <div className='nav-filters'>
+
+          <h4>Ratings</h4>
+          <h4>Streaming Service</h4>
+          <h4>Genre</h4>
+          <h4>Length</h4>
+          <h4>Media Type</h4>
         </div>
-        
-        <div class = "movies">
-            <p> this is where all the movies would go</p>
+        <div className="header-container">
+          <h1>Movie Collection</h1>
         </div>
-    </div>
-  );
+
+        <div className="movies">
+          <p> this is where all the movies would go</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
