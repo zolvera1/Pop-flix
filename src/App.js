603ba8237bb5b210627e47ad12f53b537eb0cfd5
Alison Tedford <atedford@redventures.com>
2020-06-08 15:33:29 -0400

    front end work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import React, { Component } from 'react';
 import './styles.css';
 import './css/search.css'
 import Autocomplete from "./services/search";
+import logo from './logo.svg';
+import './App.css';
+import Collapsible from 'react-collapsible'; 
+import {Card} from 'react-bootstrap'; 
 
 class App extends Component {
 
@@ -47,24 +51,51 @@ class App extends Component {
 
           <button type="button" className="btn btn-default" id="search-btn">Success</button>
         </div>
-        <div className='nav-filters'>
-
-          <h4>Ratings</h4>
-          <h4>Streaming Service</h4>
-          <h4>Genre</h4>
-          <h4>Length</h4>
-          <h4>Media Type</h4>
+        <div class='main'>
+        <div class='nav-filters'>
+          <div class="sort">
+          <h3>Filter by...</h3>
+          <hr></hr>
+            <Collapsible trigger = "Ratings" className = "filter-head">
+                <input type='range' name = 'ratings' min = '0' max = '10' ></input>
+               
+            </Collapsible><br></br>
+            <Collapsible trigger = "Streaming Services" className = "filter-head">
+              <label className="checkbox-label">
+                <input id="Netflix" type="checkbox"/><span>Netflix</span><br></br>
+                <input id="amazon" type="checkbox"/><span>Amazon Prime</span><br></br>
+                <input id="hbo" type="checkbox"/><span>HBO</span><br></br>
+                <input id="hulu" type="checkbox"/><span>Hulu</span><br></br>
+              </label>
+            </Collapsible><br></br>
+            <Collapsible trigger = "Genres" className = "filter-head">
+              <p> list off checkboxes of available genres</p>
+              </Collapsible><br></br>
+            <Collapsible trigger = "Length" className = "filter-head">
+            <input type='range' min = '0' max = '180'></input>
+              </Collapsible><br></br>
+            <Collapsible trigger = "Media Type" className = "filter-head">
+               <input id="movies" type="checkbox"/><span>Movies</span><br></br>
+                <input id="shows" type="checkbox"/><span>TV Shows</span>
+              </Collapsible><br></br>
+              </div>
         </div>
-        <div className="header-container">
+        <div class = "movie-content"> 
           <h1>Movie Collection</h1>
+          <div class="movie-grid"> 
+          <Card> 
+            <Card.Img variant = "top"/>
+            <Card.Body> 
+              <Card.Title>Movie Title</Card.Title>
+            </Card.Body>
+         </Card>
         </div>
-
-        <div className="movies">
-          <p> this is where all the movies would go</p>
         </div>
-      </div>
+       </div>
+       </div>
     );
   }
 }
+
 
 export default App;
