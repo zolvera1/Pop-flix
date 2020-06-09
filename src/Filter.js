6394776b8317a702fbe0906f3d6fc99ee34b8b28
Kyle Suter <ksuter@redventures.com>
2020-06-09 14:56:23 -0400

    Filter now pulls RV api info

--- src/Filter.js
+++ src/Filter.js
@@ -6,8 +6,43 @@ export default class Filter extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            movies: [],
-            shows: []
+
+            movies_all: [],
+            movies_considered: [],
+
+            movie_keys: [],
+            movie_titles: [],
+            movie_dates: [],
+            movie_ratings: [],
+            movie_platforms: [],
+            movie_companies: [],
+            movie_vote_counts: [],
+            movie_vote_averages: [],
+            movie_languages: [],
+            movie_pops: [],
+            //movie_genres: [],
+
+            shows_all: [],
+            shows_considered: [],
+
+            show_keys: [],
+            show_titles: [],
+            show_ratings: [],
+            show_platforms: [],
+            show_companies: [],
+            show_vote_counts: [],
+            show_vote_averages: [],
+            show_languages: [],
+            show_pops: [],
+            show_genres: [],
+
+            //available_genres: new Set(),
+
+            selected_average: [0, 10],
+            selected_services: [],
+            //selected_genres: [],
+            selected_media: [true, true] //movies, shows
+
         };
     }
 
@@ -15,11 +50,75 @@ export default class Filter extends React.Component {
         this.grabMoviesAndShows();
     }
 
+    //RV API call to grab movies/shows and store their data in arrays for parsing later
     grabMoviesAndShows() {
-        fetch('https://casecomp.konnectrv.io/movie')
+        fetch('https://casecomp.konnectrv.io/movie') 
+            .then(response => response.json()) 
+            .then(dataArray => {   
+                this.setState({ movies_all: dataArray });
+                this.setState({ movies_considered: dataArray });
+                this.setState({ movie_keys: dataArray.map(data => data.imdb)  });
+                this.setState({ movie_titles: dataArray.map(data => data.title)  });
+                this.setState({ movie_dates: dataArray.map(data => data.release_date)  });
+                this.setState({ movie_ratings: dataArray.map(data => data.rating)  });
+                this.setState({ movie_platforms: dataArray.map(data => data.streaming_platform)  });
+                this.setState({ movie_companies: dataArray.map(data => data.production_companies)  });
+                this.setState({ movie_vote_counts: dataArray.map(data => data.vote_count)  });
+                this.setState({ movie_vote_averages: dataArray.map(data => data.vote_average)  });
+                this.setState({ movie_languages: dataArray.map(data => data.original_language)  });
+                this.setState({ movie_pops: dataArray.map(data => data.popularity)  });
+
+                // //now grab all of the movies' info from IMDB and set total available genres
+                // let i;
+                // for (i = 0; i < this.state.movies_all.length; i++) {
+                //     let genres = [];
+                //     let j;
+                //     let movie_fetch = 'https://api.themoviedb.org/3/movie/' + this.state.movie_keys[i] + '?api_key=9b720ff3634dcb0e3e7923c780e028b9';
+                //     fetch(movie_fetch)
+                //         .then(result => result.json())
+                //         .then(details => {
+                //             genres = details.genres.map(deets => deets.name);
+                //             for (j = 0; j < genres.length; j++) {
+                //                 this.state.available_genres.add(genres[j]);
+                //             }
+                //         })
+                // }
+            }
+        );
+
+        fetch('https://casecomp.konnectrv.io/show')
             .then(response => response.json())
-            .then(data => this.setState({movies: data}));
-        console.log(this.state.movies);
+            .then(dataArray => {
+                this.setState({ shows_all: dataArray });
+                this.setState({ shows_considered: dataArray });
+                this.setState({ show_keys: dataArray.map(data => data.imdb)  });
+                this.setState({ show_titles: dataArray.map(data => data.title)  });
+                this.setState({ show_platforms: dataArray.map(data => data.streaming_platform)  });
+                this.setState({ show_companies: dataArray.map(data => data.production_companies)  });
+                this.setState({ show_vote_counts: dataArray.map(data => data.vote_count)  });
+                this.setState({ show_vote_averages: dataArray.map(data => data.vote_average)  });
+                this.setState({ show_languages: dataArray.map(data => data.original_language)  });
+                this.setState({ show_pops: dataArray.map(data => data.popularity)  });
+
+                //now grab all of the shows' info from IMDB and set total available genres
+                /*let i;
+                for (i = 0; i < this.state.shows_all.length; i++) {
+                    let genres = [];
+                    let j;
+                    let show_fetch = 'https://api.themoviedb.org/3/tv/tt1844624?api_key=9b720ff3634dcb0e3e7923c780e028b9'
+                    //let show_fetch = 'https://api.themoviedb.org/3/tv/' + this.state.show_keys[i] + '?api_key=9b720ff3634dcb0e3e7923c780e028b9';
+                    fetch(show_fetch)
+                        .then(result => result.json())
+                        .then(details => {
+                            genres = details.genres.map(deets => deets.name);
+                            for (j = 0; j < genres.length; j++) {
+                                this.state.available_genres.add(genres[j]);
+                            }
+                        })
+                }*/
+            }
+        );
+        //console.log(this.state.available_genres);
     }
 
     //HTML
