87946ada3b6adce008a35b9aa4cf318e75f3ed0d
Kyle Suter <ksuter@redventures.com>
2020-06-09 10:14:57 -0400

    added Filter.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import "./styles.css";
 import "./services/Search Bar Config/css/autoCompleteHome.css";
 import "./css/search.css";
 import "./App.css";
-import Collapsible from "react-collapsible";
-import { Card } from "react-bootstrap";
+
 import NavBar from "./components/navbar";
 import background from "./images/aqua-man.jpg";
+import Filter from "./Filter"
 
 import SearchBar from "./components/searchBar";
 
@@ -32,67 +32,8 @@ class App extends Component {
             <SearchBar></SearchBar>
           </div>
 
-          <div style={{ height: "140px" }}></div>
+          <Filter />
 
-          <div className="main">
-            <div className="nav-filters">
-              <div className="sort">
-                <h3>Filter by...</h3>
-                <hr></hr>
-                <Collapsible trigger="Ratings" className="filter-head">
-                  <input type="range" name="ratings" min="0" max="10"></input>
-                </Collapsible>
-                <br></br>
-                <Collapsible
-                  trigger="Streaming Services"
-                  className="filter-head"
-                >
-                  <label className="checkbox-label">
-                    <input id="Netflix" type="checkbox" />
-                    <span>Netflix</span>
-                    <br></br>
-                    <input id="amazon" type="checkbox" />
-                    <span>Amazon Prime</span>
-                    <br></br>
-                    <input id="hbo" type="checkbox" />
-                    <span>HBO</span>
-                    <br></br>
-                    <input id="hulu" type="checkbox" />
-                    <span>Hulu</span>
-                    <br></br>
-                  </label>
-                </Collapsible>
-                <br></br>
-                <Collapsible trigger="Genres" className="filter-head">
-                  <p> list off checkboxes of available genres</p>
-                </Collapsible>
-                <br></br>
-                <Collapsible trigger="Length" className="filter-head">
-                  <input type="range" min="0" max="180"></input>
-                </Collapsible>
-                <br></br>
-                <Collapsible trigger="Media Type" className="filter-head">
-                  <input id="movies" type="checkbox" />
-                  <span>Movies</span>
-                  <br></br>
-                  <input id="shows" type="checkbox" />
-                  <span>TV Shows</span>
-                </Collapsible>
-                <br></br>
-              </div>
-            </div>
-            <div className="movie-content">
-              <h1>Movie Collection</h1>
-              <div className="movie-grid">
-                <Card>
-                  <Card.Img variant="top" />
-                  <Card.Body>
-                    <Card.Title>Movie Title</Card.Title>
-                  </Card.Body>
-                </Card>
-              </div>
-            </div>
-          </div>
         </div>
       </React.Fragment>
     );
